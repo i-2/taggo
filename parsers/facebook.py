578aae96cb930eba6b79f914f93466ab41f6390a
sourcepirate <plasmashadowx@gmail.com>
2017-07-19 07:17:09 +0530

    Added more logging

--- parsers/facebook.py
+++ parsers/facebook.py
@@ -1,10 +1,14 @@
 from .base import YamlCommand, send_facebook_message, YamlExecutor
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 class FacebookCommand(YamlCommand):
     """Bot command for Facebook"""
 
     async def send(self, message, sender_id=None):
         res = await send_facebook_message(message, sender_id)
+        logger.info(res)
         return res
 
 class FacebookYamlExecutor(YamlExecutor):
