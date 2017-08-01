c03ef8adec8e698d1c617615b2828b1dfa823440
sourcepirate <plasmashadowx@gmail.com>
2017-08-01 06:40:59 +0530

    Adding response

---
+++ parsers/response.py
@@ -0,0 +1,100 @@
+"""Facebook bot response class"""
+
+import json
+from .base import make_request, FACEBOOK_ACCESS_ENDPOINT
+
+
+class FacebookBotResponse(dict):
+    """responding to facebook bot"""
+
+    MESSAGE_TYPE = None
+
+    def __init__(self, raw_dict, recipient):
+        self._response = raw_dict
+        self._recipient = recipient
+        super(FacebookBotResponse, self).__init__()
+        self._construct_payload()
+
+    def payload(self):
+        """Helps constructing payload"""
+        raise NotImplementedError("Not implemented")
+
+    def _construct_payload(self):
+        """construct the payload pocket"""
+        self["recipient"] = {"id": self._recipient}
+        self["message"] = self.payload()
+
+    async def send(self):
+        """sending response to servers"""
+        reply = await make_request(FACEBOOK_ACCESS_ENDPOINT,
+                                   method="POST",
+                                   payload=json.dumps(self))
+        return reply
+
+    @classmethod
+    def is_of(cls, typ):
+        """is type of"""
+        return cls.MESSAGE_TYPE == typ
+
+
+class TextBotResponse(FacebookBotResponse):
+    """Text Response from bot"""
+
+    MESSAGE_TYPE = "text"
+
+    def payload(self):
+        return dict(text=self._response.get("text"))
+
+
+class TemplateResponse(FacebookBotResponse):
+    """Template response"""
+
+    MESSAGE_TYPE = "template"
+    TEMPLATE_TYPE = None
+
+    def __init__(self, *args, **kwargs):
+        super(TemplateResponse, self).__init__(*args, **kwargs)
+
+    def payload(self):
+        _payload = {"template_type": self.__class__.TEMPLATE_TYPE}
+        _payload.update(**self.render())
+        return {"attachment": {
+                     "type": self.__class__.MESSAGE_TYPE, 
+                     "payload": _payload
+                 }}
+
+    def render(self):
+        """to be used by child"""
+        raise NotImplementedError()
+
+    @classmethod
+    def is_templateof(cls, temp):
+        return cls.TEMPLATE_TYPE == temp
+
+class WebViewTemplate(TemplateResponse):
+    """Adding webview template"""
+    TEMPLATE_TYPE = "generic"
+
+    def render(self):
+        return self._response.get("elements", [])
+
+
+class ListTemplate(WebViewTemplate):
+    """Adding list template"""
+    TEMPLATE_TYPE = "list"
+
+
+def get_response(recipient, response):
+    """get the appropriate response object"""
+
+    _type = response.get("type")
+    _tt = response.get("template_type")
+
+    if _type == "text":
+        return TextBotResponse(response, recipient)
+    elif  _tt == "template":
+        if _tt == "generic":
+            return WebViewTemplate(response, recipient)
+        elif _tt == "list":
+            return ListTemplate(response, recipient)
+    return None
